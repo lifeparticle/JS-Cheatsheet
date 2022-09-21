d1b0cfe8455e397a7d1e150eeab44563e73d775c
Mahbub Zaman <mahbubzaman17@gmail.com>
2022-09-21 14:59:29 +1000

    Create index.js

---
+++ challenges/28/index.js
@@ -0,0 +1,42 @@
+let todos = [
+	{
+		text: "todo 1",
+		completed: true,
+	},
+	{
+		text: "todo 2",
+		completed: true,
+	},
+	{
+		text: "todo 3",
+		completed: true,
+	},
+	{
+		text: "todo 4",
+		completed: true,
+	},
+	{
+		text: "todo 5",
+		completed: false,
+	},
+];
+
+const getThingsToDo = (todos) => {
+	return todos.filter((todo, index) => {
+		return todo.completed === false;
+	});
+};
+
+const summary = document.createElement("h2");
+summary.textContent = `You have ${getThingsToDo(todos).length} todos left`;
+document.querySelector("body").appendChild(summary);
+
+todos.forEach((todo) => {
+	const p = document.createElement("p");
+	p.textContent = todo.text;
+	document.querySelector("body").appendChild(p);
+});
+
+document.querySelector("button").addEventListener("click", (e) => {
+	console.log(`Add todo button is clicked`);
+});
