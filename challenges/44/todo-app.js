a796aac20fa28cca5c89958ee454448655378d56
Mahbub Zaman <mahbubzaman17@gmail.com>
2022-09-29 15:40:49 +1000

    Add files via upload

---
+++ challenges/44/todo-app.js
@@ -0,0 +1,30 @@
+let todos = getSavedTodos()
+
+const filters = {
+    searchText: '',
+    hideCompleted: false
+}
+
+renderTodos(todos, filters)
+
+document.querySelector('#search-text').addEventListener('input', (e) => {
+    filters.searchText = e.target.value
+    renderTodos(todos, filters)
+})
+
+document.querySelector('#new-todo').addEventListener('submit', (e) => {
+    e.preventDefault()
+    todos.push({
+        id: uuidv4(),
+        text: e.target.elements.text.value,
+        completed: false
+    })
+    saveTodos(todos)
+    renderTodos(todos, filters)
+    e.target.elements.text.value = ''
+})
+
+document.querySelector('#hide-completed').addEventListener('change', (e) => {
+    filters.hideCompleted = e.target.checked
+    renderTodos(todos, filters)
+})
