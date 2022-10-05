0f1b093747a33801c5fa37b9103ce20b25e42f26
Mahbub Zaman <mahbubzaman17@gmail.com>
2022-10-05 16:12:00 +1100

    Add files via upload

---
+++ challenges/60/requests.js
@@ -0,0 +1,28 @@
+const getPuzzle = (wordCount) => {
+    return fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`).then((response) => {
+        if (response.status === 200) {
+            return response.json()
+        } else {
+            throw new Error('Unable to fetch puzzle')
+        }
+    }).then((data) => {
+        return data.puzzle
+    })
+}
+
+const getCountry = (countryCode) => new Promise((resolve, reject) => {
+    const countryRequest = new XMLHttpRequest()
+
+    countryRequest.addEventListener('readystatechange', (e) => {
+        if (e.target.readyState === 4 && e.target.status === 200) {
+            const data = JSON.parse(e.target.responseText)
+            const country = data.find((country) => country.alpha2Code === countryCode)
+            resolve(country)
+        } else if (e.target.readyStatet === 4) {
+            reject('Unable to fetch data')
+        }
+    })
+
+    countryRequest.open('GET', 'http://restcountries.eu/rest/v2/all')
+    countryRequest.send()
+})
