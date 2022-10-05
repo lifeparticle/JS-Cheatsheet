5b5c2167296d7876ea83c218194429c916f16f4d
Mahbub Zaman <mahbubzaman17@gmail.com>
2022-10-05 16:46:49 +1100

    Add files via upload

---
+++ challenges/64/requests.js
@@ -0,0 +1,36 @@
+const getPuzzle = async (wordCount) => {
+    const response = await fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
+    
+    if (response.status === 200) {
+        const data = await response.json()
+        return data.puzzle
+    } else {
+        throw new Error('Unable to get puzzle')
+    }
+}
+
+const getCurrentCountry = async () => {
+    const location = await getLocation()
+    return getCountry(location.country)
+}
+
+const getCountry = async (countryCode) => {
+    const response = await fetch('http://restcountries.eu/rest/v2/all')
+
+    if (response.status === 200) {
+        const data = await response.json()
+        return data.find((country) => country.alpha2Code === countryCode)
+    } else {
+        throw new Error('Unable to fetch the country')
+    }
+}
+
+const getLocation = async () => {
+    const response = await fetch('http://ipinfo.io/json?token=1a11bd55cc8f9c')
+
+    if (response.status === 200) {
+        return response.json()
+    } else {
+        throw new Error('Unable to get the current location')
+    }
+}
