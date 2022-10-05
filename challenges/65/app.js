c5c27c8aac3b8762899b46ae663835c4e9315f06
Mahbub Zaman <mahbubzaman17@gmail.com>
2022-10-05 16:55:17 +1100

    Add files via upload

---
+++ challenges/65/app.js
@@ -0,0 +1,36 @@
+const puzzleEl = document.querySelector('#puzzle')
+const guessesEl = document.querySelector('#guesses')
+let game1
+
+window.addEventListener('keypress', (e) => {
+    const guess = String.fromCharCode(e.charCode)
+    game1.makeGuess(guess)
+    render()
+})
+
+const render = () => {
+    puzzleEl.textContent = game1.puzzle
+    guessesEl.textContent = game1.statusMessage
+}
+
+const startGame = async () => {
+    const puzzle = await getPuzzle('2')
+    game1 = new Hangman(puzzle, 5)
+    render()
+}
+
+document.querySelector('#reset').addEventListener('click', startGame)
+
+startGame()
+
+// getPuzzle('2').then((puzzle) => {
+//     console.log(puzzle)
+// }).catch((err) => {
+//     console.log(`Error: ${err}`)
+// })
+
+// getCurrentCountry().then((country) => {
+//     console.log(country.name)
+// }).catch((error) => {
+//     console.log(error)
+// })
